68666f77b4212b61f1da66922907d0f613471e8d
AsfaaKhan <khanzadiasfi0314@gmail.com>
2024-11-06 23:25:53 +0500

    A Section of Website , covert the figma design

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+
+const Header = () =>{
+    return(
+        <div className = "h-[134px] bg-[#A29875] ">
+            <h1 className="font-normal text-[75px] text-[#FFFFFF] ml-9 font-serif">MANZZARI</h1>
+        </div>
+    )
+}
+export default Header
